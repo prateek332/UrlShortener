b8cedfaa319874daa0eb1e668f89aa4b7716e4a9
prateek332 <prateekn332@gmail.com>
2022-01-16 21:24:29 +0530

    Initial Commit

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortner.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            return Ok("API is up and running");
+        }
+    }
+}
