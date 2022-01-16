1133b6a034d21f9e295e616b47dedfef2ded8511
prateek332 <prateekn332@gmail.com>
2022-01-16 23:07:51 +0530

    Shortening of url working User can make with a url to shorten it, and they receive a json reponse with shortened url. Url is cached in memory, so if user makes another request with same url, they get back the same shortened url in response.

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UrlShortner.Services;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace UrlShortner.Controllers
 {
@@ -8,9 +9,12 @@ namespace UrlShortner.Controllers
     [ApiController]
     public class ApiController : ControllerBase
     {
-        IRandomString _randomString;
-        public ApiController(IRandomString randomString)
+        const int randomUrlStringLength = 7;
+        readonly IRandomString _randomString;
+        readonly IMemoryCache _memoryCache;
+        public ApiController(IMemoryCache memoryCache, IRandomString randomString)
         {
+            _memoryCache = memoryCache;
             _randomString = randomString;
         }
 
@@ -20,13 +24,39 @@ namespace UrlShortner.Controllers
             return Ok("API is up and running");
         }
 
-        [HttpGet("/shorten-url")]
-        public IActionResult ShortenUrl([FromQuery] string urlToShorten)
+        [HttpGet("shorten-url/{urlToShorten}")]
+        public async Task<IActionResult> ShortenUrl([FromRoute] string urlToShorten)
         {
-            Console.WriteLine(_randomString.GetRandomString(10));
-            Console.WriteLine(urlToShorten);
-            return Ok();
+            // get the cached entry or create a new entry if cache doesn't exists
+            var cacheEntry = await _memoryCache.GetOrCreateAsync(urlToShorten, entry =>
+            {
+                // generate a random string for appending at the end of shortened url
+                string randomString = _randomString.GetRandomString(randomUrlStringLength);
+
+                // cached entry would be removed after 24hrs
+                entry.SlidingExpiration = TimeSpan.FromDays(1);
+
+                return Task.FromResult(randomString);
+            });
+
+            // if no cached entry was found, then create another cache with
+            // (key, value) = (cache, urlToShorten), so when user request to shortened url
+            // the user is redirected to cached url
+            if (cacheEntry.Length == 0)
+            {
+                _memoryCache.Set(cacheEntry, urlToShorten);
+            }
+
+            string shortenUrl = generateShortenUrl(cacheEntry);
+            return Ok(new
+            {
+                shortenUrl = shortenUrl,
+            });
         }
 
+        private string generateShortenUrl(string randomString)
+        {
+            return $"https://{Request.Host.ToUriComponent()}/{randomString}";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IRandomString, RandomString>();
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
